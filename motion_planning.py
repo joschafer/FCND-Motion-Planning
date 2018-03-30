de044829e487a7431cf6a3575de8829140a2433c
Jeff Schafer <j_o_schafer@yahoo.com>
2018-03-30 14:22:04 -0700

    Manually sync changes not in github

--- motion_planning.py
+++ motion_planning.py
@@ -11,6 +11,9 @@ from udacidrone.connection import MavlinkConnection
 from udacidrone.messaging import MsgID
 from udacidrone.frame_utils import global_to_local
 
+# Import additional utilities
+from planning_utils_additions import prune_path
+import time
 
 class States(Enum):
     MANUAL = auto()
@@ -120,36 +123,72 @@ class MotionPlanning(Drone):
         self.target_position[2] = TARGET_ALTITUDE
 
         # TODO: read lat0, lon0 from colliders into floating point values
-        
+        with open('colliders.csv') as f:
+            first_line = f.readline().strip()
+        (_, lat0, _, _, lon0) = first_line.replace(' ',',').split(',')
+        # print("DEBUG: first_line {}, lat0 {}, lon0 {}".format(first_line, lat0, lon0))
+
         # TODO: set home position to (lon0, lat0, 0)
+        self.set_home_position(float(lon0), float(lat0), 0)
+        # print("DEBUG: home position {}".format(self.global_home))
 
         # TODO: retrieve current global position
- 
+        start_global_position = (self._longitude, self._latitude, self._altitude)
+        # print("DEBUG: Global position {}".format(start_global_position))
+
         # TODO: convert to current local position using global_to_local()
-        
+        start_local_position = global_to_local(start_global_position, self.global_home)
+        # print("DEBUG: local position {}".format(start_local_position))
+
         print('global home {0}, position {1}, local position {2}'.format(self.global_home, self.global_position,
                                                                          self.local_position))
         # Read in obstacle map
         data = np.loadtxt('colliders.csv', delimiter=',', dtype='Float64', skiprows=2)
-        
+
         # Define a grid for a particular altitude and safety margin around obstacles
         grid, north_offset, east_offset = create_grid(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
         print("North offset = {0}, east offset = {1}".format(north_offset, east_offset))
+        # print("DEBUG: grid shape {}".format(grid.shape))
+
         # Define starting point on the grid (this is just grid center)
-        grid_start = (-north_offset, -east_offset)
+        # grid_start = (-north_offset, -east_offset)
         # TODO: convert start position to current position rather than map center
-        
+        grid_start = (-north_offset + int(start_local_position[0]), -east_offset + int(start_local_position[1]))
+
         # Set goal as some arbitrary position on the grid
-        grid_goal = (-north_offset + 10, -east_offset + 10)
+        # grid_goal = (-north_offset + 10, -east_offset + 10)
         # TODO: adapt to set goal as latitude / longitude position and convert
+        gl_grid_goal = (-122.395989, 37.795227,0)
+        gl_grid_x_y = global_to_local(gl_grid_goal, self.global_home)
+        grid_goal = (-north_offset + int(gl_grid_x_y[0]), -east_offset + int(gl_grid_x_y[1]))
 
         # Run A* to find a path from start to goal
         # TODO: add diagonal motions with a cost of sqrt(2) to your A* implementation
         # or move to a different search space such as a graph (not done here)
         print('Local Start and Goal: ', grid_start, grid_goal)
+        # Exit if goal and start are the same position
+        if (grid_start == grid_goal):
+            print ('Start and goal are equal.  Exiting')
+            sys.exit()
+
+        # DEBUG: start_time = time.time()
         path, _ = a_star(grid, heuristic, grid_start, grid_goal)
+        # DEBUG: elapsed_time = time.time() - start_time
+        #Exit if plan can't be found.
+        if (len(path) == 0):
+            print ('Exiting')
+            sys.exit()
+
+        # print("DEBUG: Plan ET {} MS".format(1000.0 * elapsed_time))
+
         # TODO: prune path to minimize number of waypoints
         # TODO (if you're feeling ambitious): Try a different approach altogether!
+        # print("DEBUG: path before {}".format(len(path)))
+        # DEBUG: start_time = time.time()
+        path = prune_path(path)
+        # DEBUG: elapsed_time = time.time() - start_time
+        # print("DEBUG: Plan ET {} MS".format(1000.0 * elapsed_time))
+        # print("DEBUG: path after {}".format(len(path)))
 
         # Convert path to waypoints
         waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in path]
