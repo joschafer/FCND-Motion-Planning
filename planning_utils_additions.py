85d4f4ea7631a82809690326ca837965e2e5cf1d
Jeff Schafer <j_o_schafer@yahoo.com>
2018-03-30 15:06:48 -0700

    Cleanup prune_path

--- planning_utils_additions.py
+++ planning_utils_additions.py
@@ -8,27 +8,19 @@ def collinearity_check(p1, p2, p3, epsilon=1e-6):
     det = np.linalg.det(m)
     return abs(det) < epsilon
 
-# We're using collinearity here, but you could use Bresenham as well!
+# From previous A* work
 def prune_path(path):
     pruned_path = [p for p in path]
     # TODO: prune the path!
-
-    i = 0
+    i=0
     while i < len(pruned_path) - 2:
-        p1 = point(pruned_path[i])
-        p2 = point(pruned_path[i+1])
-        p3 = point(pruned_path[i+2])
-
-        # If the 3 points are in a line remove
-        # the 2nd point.
-        # The 3rd point now becomes and 2nd point
-        # and the check is redone with a new third point
-        # on the next iteration.
-        if collinearity_check(p1, p2, p3):
-            # Something subtle here but we can mutate
-            # `pruned_path` freely because the length
-            # of the list is check on every iteration.
+        start = point(pruned_path[i])
+        middle = point(pruned_path[i+1])
+        end = point(pruned_path[i+2])
+        if collinearity_check(start, middle, end):
+            # remove middle
             pruned_path.remove(pruned_path[i+1])
         else:
             i += 1
+
     return pruned_path
